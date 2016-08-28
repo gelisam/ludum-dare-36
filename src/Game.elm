38edd7d6b4b9b8a3a452fa87032a95115c13f3c7
Samuel Gélineau <gelisam+github@gmail.com>
2016-08-28 13:43:46 -0400

    images for all buttons

--- src/Game.elm
+++ src/Game.elm
@@ -4,6 +4,7 @@ import Html exposing (Html)
 import Html.App as App
 import Html.Attributes as Html
 import Html.Events as Events
+import String
 
 
 type alias Model =
@@ -49,24 +50,30 @@ update msg model =
       { model | blackCount = model.blackCount + 1 }
 
 
-button : msg -> String -> Html msg
-button msg text =
-  Html.button [Events.onClick msg] [Html.text text]
+button : Bool -> msg -> String -> Html msg
+button enabled msg text =
+  Html.div
+    [ Html.class (String.join " " [if enabled then "enabled" else "disabled", "button"])
+    ]
+  [ Html.img
+      [ Html.src ("img/" ++ text ++ ".png")
+      , Events.onClick msg
+      ]
+    []
+  , Html.div []
+    [ Html.text text
+    ]
+  ]
 
 view : Model -> Html Msg
 view model =
   Html.div []
-  [ button NextLevel                "Next Level"
-  , button ResetLevel               "Reset Level"
-  , Html.img
-      [ Html.class "enabled button"
-      , Html.src "img/ragrog.png"
-      , Events.onClick IncreaseButtonCount
-      ]
-    []
-  , button IncreaseYellowCount      "Yellow +N"
-  , button IncreaseYellowMultiplier "N +1"
-  , button IncreaseBlackCount       "Black +1"
+  [ button False NextLevel                "right"
+  , button True  ResetLevel               "undo"
+  , button True  IncreaseButtonCount      "ragrog"
+  , button True  IncreaseYellowCount      "bilp"
+  , button True  IncreaseYellowMultiplier "blagar"
+  , button True  IncreaseBlackCount       "barif"
   ]
 
 
